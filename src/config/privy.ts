82294f69ed961e550aa219f971dd570a85f5db9f
Maxence <rafalovic.pro@gmail.com>
2025-01-30 16:41:55 +0100

    commit for vercel

--- src/config/privy.ts
+++ src/config/privy.ts
@@ -1,10 +1,13 @@
-import { PrivyClientConfig } from '@privy-io/react-auth'
-
-export const PRIVY_CONFIG: Partial<PrivyClientConfig> = {
+// src/config/privy.ts
+export const PRIVY_CONFIG = {
+  appId: process.env.NEXT_PUBLIC_PRIVY_APP_ID as string,
+  
   appearance: {
     theme: 'dark' as const,
     accentColor: '#4F46E5',
     showWalletLoginFirst: false,
   },
-  loginMethods: ['twitter'],
+
+  // On change pour Discord
+  loginMethods: ['discord'],
 }
